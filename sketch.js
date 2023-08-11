186952bbce303ff364336c5ef02ce6cb5d244f12
Nicolly <141414263+nicollyrestelatto@users.noreply.github.com>
2023-08-11 08:18:27 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(600, 600);
+ ;background ('pink')
+}
+  
+function draw() {
+  
+  
+  stroke('purple')
+fill ('black');
+  
+  
+    if(mouseIsPressed){
+ rect(mouseX, mouseY,50,30);
+    }
+
+}
